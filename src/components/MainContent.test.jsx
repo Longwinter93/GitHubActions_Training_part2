dd1007f1974e3869b540b63af7e5a083e660d8c2
Lukasz <dlugozima1993@gmail.com>
2025-07-01 20:09:44 +0200

    added comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 // My tests
+//Add yet another comment
 import MainContent from './MainContent';
 
 describe('MainContent', () => {
